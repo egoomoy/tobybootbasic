4f7ac115417d7e3b8ea1214351ed2cc3fe44bfc0
EdwinGoo <egoomoy@gmail.com>
2023-06-18 16:22:30 +0900

    커스텀@Conditional

--- src/main/java/com/excellent/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/excellent/config/EnableMyAutoConfiguration.java
@@ -10,13 +10,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Import(MyAutoConfigImportSelector.class)
-public @interface EnableMyAutoConfiguration{
+public @interface EnableMyAutoConfiguration {
 }
 
-echo "# tobybootbasic" >> README.md
-        git init
-        git add README.md
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin git@github.com:egoomoy/tobybootbasic.git
-        git push -u origin main
--- src/main/java/com/excellent/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/excellent/config/autoconfig/TomcatWebServerConfig.java
@@ -1,28 +1,25 @@
 package com.excellent.config.autoconfig;
 
+import com.excellent.config.ConditionalMyOnClass;
 import com.excellent.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatConfition.class)
+//@Conditional(TomcatWebServerConfig.TomcatConfition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
 
-    static class TomcatConfition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            boolean present = ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
-            return present;
-        }
-    }
+//    static class TomcatConfition implements Condition {
+//        @Override
+//        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//            boolean present = ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
+//            return present;
+//        }
+//    }
 }
