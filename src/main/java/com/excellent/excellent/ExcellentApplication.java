7c9c72943467299027bd2a07ba99f973b5db65cf
EdwinGoo <egoomoy@gmail.com>
2023-06-19 23:37:36 +0900

    자동 구성에 Environment 프로퍼티 적용

--- src/main/java/com/excellent/excellent/ExcellentApplication.java
+++ src/main/java/com/excellent/excellent/ExcellentApplication.java
@@ -1,13 +1,25 @@
 package com.excellent.excellent;
 
 import com.excellent.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 
 //@SpringBootApplication
 @MySpringBootApplication
 public class ExcellentApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ExcellentApplication.class, args);
-	}
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println(name);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcellentApplication.class, args);
+    }
 }
--- src/main/java/com/excellent/excellent/WebServerConfiguration.java
+++ src/main/java/com/excellent/excellent/WebServerConfiguration.java
@@ -1,16 +1,11 @@
 package com.excellent.excellent;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration {
-    @Bean
-    ServletWebServerFactory customServletWebServerFactory() {
-        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        tomcatServletWebServerFactory.setPort(9090);
-        return tomcatServletWebServerFactory;
-    }
+//    @Bean
+//    ServletWebServerFactory customServletWebServerFactory() {
+//        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+//        tomcatServletWebServerFactory.setPort(9090);
+//        return tomcatServletWebServerFactory;
+//    }
 }
